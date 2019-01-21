f1ae61301b8745ed236a4962e5aea41c7235f9d8
jeewoong.han <user@AL01277083.local>
2019-01-21 21:03:20 +0900

    add project

---
+++ MealDiary/MealDiary/Controller/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MealDiary
+//
+//  Created by jeewoong.han on 21/01/2019.
+//  Copyright © 2019 clap. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+}
+
+extension MainViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
