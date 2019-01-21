8f47417c9528634902b33ec8a1f5d0c03cfaf8f0
wooooooong <hjw01234@gmail.com>
2019-01-22 00:24:59 +0900

    add scorllview velocity animation

--- MealDiary/MealDiary/Controller/MainViewController.swift
+++ MealDiary/MealDiary/Controller/MainViewController.swift
@@ -13,14 +13,19 @@ class MainViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     var headerView: MainHeaderView?
     var cards: [Card] = []
+    var tableFrame: CGRect?
+    var headerFrame: CGRect?
+    let headerViewHeight: CGFloat = 84
     
     func setUpInitialView() {
-        guard let headerView = MainHeaderView.instanceFromNib() else { return }
+        headerView = MainHeaderView.instanceFromNib()
         
         let statusBarHeight = UIApplication.shared.statusBarFrame.height
         let origin = CGPoint(x: 0, y: statusBarHeight + 50)
-        let frame = CGRect(origin: origin, size: CGSize(width: self.view.frame.width, height: 84))
-        headerView.setUp(frame: frame)
+        headerFrame = CGRect(origin: origin, size: CGSize(width: self.view.frame.width, height: headerViewHeight))
+        headerView?.setUp(frame: headerFrame ?? CGRect(origin: .zero, size: .zero))
+        
+        guard let headerView = headerView else { return }
         self.view.addSubview(headerView)
     }
     
@@ -32,9 +37,37 @@ class MainViewController: UIViewController {
         tableView.register(UINib(nibName: MainCardTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: MainCardTableViewCell.identifier)
         
         let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        let origin = CGPoint(x: 0, y: statusBarHeight + 60 + headerView.frame.height + 10)
+        let origin = CGPoint(x: 0, y: statusBarHeight + 60 + headerView.frame.height)
         let size = CGSize(width: self.view.frame.width, height: self.view.frame.height - origin.y)
-        tableView.frame = CGRect(origin: origin, size: size)
+        tableFrame = CGRect(origin: origin, size: size)
+        tableView.frame = tableFrame ?? CGRect(origin: .zero, size: .zero)
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView,
+                                            withVelocity velocity: CGPoint,
+                                            targetContentOffset: UnsafeMutablePointer<CGPoint>){
+        if velocity.y > 0 {
+            // scroll down
+            UIView.animate(withDuration: 0.3) {
+                guard let tableFrame = self.tableFrame else { return }
+                guard let headerFrame = self.headerFrame else { return }
+                
+                self.headerView?.alpha = 0
+//                self.headerView?.frame = CGRect(x: 0, y: headerFrame.origin.y - self.headerViewHeight, width: headerFrame.width, height: headerFrame.height)
+                self.tableView.frame = CGRect(x: 0, y: tableFrame.origin.y - self.headerViewHeight, width: tableFrame.size.width, height: tableFrame.size.height + self.headerViewHeight)
+            }
+            
+        } else if velocity.y < 0 {
+            // scroll up
+            UIView.animate(withDuration: 0.3) {
+                guard let tableFrame = self.tableFrame else { return }
+                guard let headerFrame = self.headerFrame else { return }
+                
+                self.headerView?.alpha = 1
+//                self.headerView?.frame = CGRect(x: 0, y: headerFrame.origin.y, width: headerFrame.width, height: headerFrame.height)
+                self.tableView.frame = CGRect(x: 0, y: tableFrame.origin.y, width: tableFrame.size.width, height: tableFrame.size.height)
+            }
+        }
     }
 }
 
@@ -48,6 +81,7 @@ extension MainViewController {
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return cards.count
         return 10
     }
     
@@ -74,5 +108,5 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 45
     }
-    
 }
+
